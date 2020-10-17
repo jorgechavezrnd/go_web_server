5ad8d349640bc1634ae68480f32a2db81d13a32d
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-10-17 12:52:36 -0400

    Handler for UserPostRequest added, and User struct modified for use lowercase in json

--- handlers.go
+++ handlers.go
@@ -24,3 +24,20 @@ func PostRequest(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Payload %v\n", metadata)
 }
+
+func UserPostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "error: %v", err)
+		return
+	}
+	response, err := user.ToJson()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
